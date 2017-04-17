cebe72aeab97797fdbb1663efdca91602f11cec3
PDC <poodarchu@gmail.com>
2017-04-17 15:52:52 +0800

    add definition of data operations, include calculate entropy, mse ...

---
+++ utils/data_operate.py
@@ -0,0 +1,34 @@
+from __future__ import division
+import numpy as np
+
+# Calculate entropy of label array y
+def entropy(y):
+    pass
+
+# Calculate mean square error between y_true and y_predict
+def mean_square_error(y_true, y_pred):
+    pass
+
+# Calculate variance of features in data set X
+def variance(X):
+    pass
+
+# Calculate standard deviations of features in data set X
+def standard_deviation(X):
+    pass
+
+# Calculate distance between two vectors
+def euclidean_distance(v1, v2):
+    pass
+
+# Calculate accuracy given y_true and y_predict
+def accuracy_score(y_true, y_pred):
+    pass
+
+# Calculate the variance matrix of data set X
+def variance_matrix():
+    pass
+
+# Calculate correlation matrix of data set X
+def correlation_matrix():
+    pass
