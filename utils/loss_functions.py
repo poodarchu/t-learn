19e3d26ed021d0507069b45221678585b7fa75b5
PDC <poodarchu@gmail.com>
2017-04-17 15:01:07 +0800

    add 5 types of loss functions.

---
+++ utils/loss_functions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+import numpy as np
+
+
+'''
+    机器学习算法一般都是对损失函数 loss function 求最优解。
+    大部分损失函数都包含两项，损失误差 loss term 以及正则项 regularization term。
+'''
+
+# Gold Standard Loss is often called 0-1 loss
+def gold_standard_loss(**kwargs):
+    pass
+
+def hinge_loss():
+    pass
+
+# logistic loss
+def cross_entropy_loss():
+    pass
+
+class SquareLoss():
+    def __init__(self): pass
+
+def exponential_loss():
+    pass
